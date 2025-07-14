bfe43f6a19792773960282cfe8ed6a2ea8851f8f
MarcoSchafer <marcoschafer02@gmail.com>
2025-07-14 22:44:36 +0200

    Finalised mario-more

--- pset1/mario-more/mario.c
+++ pset1/mario-more/mario.c
@@ -12,5 +12,32 @@ int main (void)
         scanf("%i", &height);
     } while (height < 1 || height > 8);
     
-    printf("%i", height);
+    //Print rows
+    for (int i = 0; i < height; i++)
+    {
+        //Print spaces
+        for (int s = 0; s < height - i - 1;s++)
+        {
+            printf(" ");
+        }
+
+        //Print blocks
+        for (int b = 0; b <= i; b++)
+        {
+            printf("#");
+        }
+
+        //Print column gap
+        printf("  ");
+
+        //Print second traingle
+        for (int j = height + i + 1; j > height; j--)
+        {
+            printf("#");
+        }
+        
+        //Print new line
+        printf("\n");
+    }
+
 }
