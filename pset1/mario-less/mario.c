57edbda44913f62f45f3503314698510a163ca6c
MarcoSchafer <marcoschafer02@gmail.com>
2025-07-14 14:29:51 +0200

    Added mario-less.c

--- pset1/mario-less/mario.c
+++ pset1/mario-less/mario.c
@@ -2,5 +2,31 @@
 
 int main (void)
 {
-   printf("#");
+    //Declare variables
+    int height;
+
+    //Get user input for height of pyramid
+    do
+    {
+        printf("Height: ");
+        scanf("%i", &height);
+    } while (height < 1 || height > 8);
+
+    //Print rows    
+    for (int i = 0; i < height; i++)
+    {
+        //Print spaces
+        for (int s = 0; s < height - i - 1; s++)
+        {
+            printf(" ");
+        }
+        //Print blocks
+        for (int b = 0; b <= i; b++)
+        {
+            printf("#");
+        }
+        //New line
+        printf("\n");
+    }
+
 }
